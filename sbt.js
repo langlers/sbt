acd7c160244c689e427d49516d2e70b7fb499732
Daniel Bond <daniel@langlers.com>
2020-11-09 17:03:48 -0800

    try different jquery dom load init strategy.

--- sbt.js
+++ sbt.js
@@ -1,17 +1,19 @@
 //console.log('sbt.js');
 
 
-jQuery(function($){
+//jQuery(function($){
     /* jQuery isolated and on dom ready. */
 
+
+$(document).ready(function() {
     // init Isotope
         var $grid = $('.card-deck').isotope({
             itemSelector: '.card',
             layoutMode: 'fitRows'
         });
     // layout Isotope after each image loads
-        $grid.imagesLoaded().progress( function() {
+       /* $grid.imagesLoaded().progress( function() {
             $grid.isotope('layout');
-        });
+        }); */
 });
 
