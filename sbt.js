b1a7d4521bf2148b41116e6fd5ad739791e37344
Daniel Bond <daniel@langlers.com>
2020-11-09 16:53:02 -0800

    get started with isotope for filtering and sorting.

--- sbt.js
+++ sbt.js
@@ -1,1 +1,17 @@
 //console.log('sbt.js');
+
+
+jQuery(function($){
+    /* jQuery isolated and on dom ready. */
+
+    // init Isotope
+        var $grid = $('.card-deck').isotope({
+            itemSelector: '.card',
+            layoutMode: 'fitRows'
+        });
+    // layout Isotope after each image loads
+        $grid.imagesLoaded().progress( function() {
+            $grid.isotope('layout');
+        });
+});
+
