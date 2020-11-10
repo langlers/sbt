70e1f130e8dfee46d59aba431d67bb9415e5dbbd
Daniel Bond <daniel@langlers.com>
2020-11-09 17:41:54 -0800

    add filter button js.

--- sbt.js
+++ sbt.js
@@ -7,13 +7,19 @@
 
 $(document).ready(function() {
     // init Isotope
-        var $grid = $('.card-deck').isotope({
-            itemSelector: '.card',
-            layoutMode: 'fitRows'
-        });
+    var $grid = $('.card-deck').isotope({
+        itemSelector: '.card',
+        layoutMode: 'fitRows'
+    });
     // layout Isotope after each image loads
        /* $grid.imagesLoaded().progress( function() {
             $grid.isotope('layout');
         }); */
+
+    // filter items on button click
+    $('.filters-button-group').on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
 });
 
