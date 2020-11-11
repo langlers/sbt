ca299e6a463646adededb7493b8eb49526b46fca
Daniel Bond <daniel@langlers.com>
2020-11-11 13:46:46 -0800

    article img grid...

--- js/articles.js
+++ js/articles.js
@@ -6,30 +6,35 @@
 
 	/* Set body to half width and move images to right col. */
 	var $imgCol = $('<div class="img-grid col-lg-6">');
-	$articleBody.removeClass('w-100 mt-5 d-inline-block').addClass('col-lg-6');
 	$articleBody.after($imgCol);
-	$articleBody.find('img').each(function(){
-		$(this).appendTo($imgCol).wrap('<div class="grid-item col-md-6"><a class="popup-gallery" href="' + $(this).attr('src') + '">');
-	});
+	$articleBody.removeClass('w-100 mt-5 d-inline-block').addClass('col-lg-6');
 	$article.find('.col-lg-6').wrapAll('<div class="row">');
+
+	/* iterate over images, then run isotope */
+	var $images = $articleBody.find('img');
+	var count = $images.length;
+	$images.each(function(){
+		$(this).appendTo($imgCol).wrap('<div class="grid-item col-md-6"><a class="popup-gallery" href="' + $(this).attr('src') + '">');
+		if (!--count) {
+			/* Set up Isotope Layout for Images */
+			var $grid = $('.img-grid').isotope({
+				itemSelector: '.grid-item',
+				layoutMode: 'fitRows',
+				masonry: {
+					columnWidth: '.grid-item'
+				}
+			});
+			$grid.imagesLoaded(function(){
+				$grid.isotope();
+			});
+		};
+	});
 
 	/* Wrap first sentence */
 	var $firstP = $article.find('.article-body p:first');
 	var parts = $firstP.html().split('.');
 	var firstSentence = '<span class="first-sentence">' + parts.shift() + '.</span>';
 	$firstP.html(firstSentence + parts.join('.'));
-
-	/* Set up Isotope Layout for Images */
-	var $grid = $('.img-grid').isotope({
-		itemSelector: '.grid-item',
-		layoutMode: 'fitRows',
-		masonry: {
-			columnWidth: '.grid-item'
-		}
-	});
-	$grid.imagesLoaded(function(){
-		$grid.isotope();
-	});
 
 	/* Create popup gallery from images */
 	$('.popup-gallery').magnificPopup({
@@ -38,6 +43,7 @@
 			enabled: true
 		}
 	});
+
 
 
 })(jQuery);
