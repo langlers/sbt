a0e49b65c5a0017fe0ac1380ab1092c487c84f47
Daniel Bond <daniel@langlers.com>
2020-11-11 12:32:27 -0800

    article layout imgs

--- js/articles.js
+++ js/articles.js
@@ -3,10 +3,17 @@
 
 	var $article = $('body.articles .article-container');
 
+	/* Set body to half width and move images to right col. */
+	var $imgCol = $('<div class="img-grid col-lg-6">');
+	$article.find('.article-body').removeClass('w-100').addClass('col-lg-6').insertAfter($imgCol);
+	$article.find('img').each(function(){
+		$(this).appendTo($imgCol);
+	});
+
 	/* Wrap first sentence */
 	var $firstP = $article.find('.article-body p:first');
 	var parts = $firstP.html().split('.');
-	var firstSentence = '<span class="first-sentence">' + parts.shift() + '</span>';
+	var firstSentence = '<span class="first-sentence">' + parts.shift() + '.</span>';
 	$firstP.html(firstSentence + parts.join('.'));
 
 
